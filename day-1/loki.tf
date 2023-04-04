5e7d88d5c4d1bc89019616b65784ef475f8453d7
Martins Eglitis <sitilge@gmail.com>
2023-04-04 13:34:34 +0300

    Create a GCS bucket for Loki

--- day-1/loki.tf
+++ day-1/loki.tf
@@ -10,3 +10,13 @@ resource "google_service_account_iam_binding" "loki" {
   role               = "roles/iam.workloadIdentityUser"
   service_account_id = google_service_account.loki.name
 }
+
+resource "google_storage_bucket" "loki" {
+  location      = var.google_region
+  name          = "${var.name}-loki"
+  storage_class = "REGIONAL"
+
+  autoclass {
+    enabled = true
+  }
+}
