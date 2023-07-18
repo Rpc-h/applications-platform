5b67169ec5db0df9fe13461db59adf25cefcce98
Ronny Esterluss <esterlus@users.noreply.github.com>
2023-07-18 10:33:02 +0200

    initial nix setup (#59)
    * initial flake
    
    * added application deps

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  linuxPkgs = with pkgs; lib.optional stdenv.isLinux (
+    inotify-tools
+  );
+  macosPkgs = with pkgs; lib.optional stdenv.isDarwin (
+    with darwin.apple_sdk.frameworks; [
+      CoreFoundation
+      CoreServices
+    ]
+  );
+in
+with pkgs;
+mkShell {
+  nativeBuildInputs = [
+    (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+    kubectl
+
+    # custom pkg groups
+    linuxPkgs
+    macosPkgs
+  ];
+}
